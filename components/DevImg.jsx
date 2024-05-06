d46c62a7a0ac9b8cc6dac45eddfe30c60746b4e6
jnorgini <jnorgini@gmail.com>
2024-05-06 20:32:08 -0300

    feat: socials, devImg and badge in hero

--- components/DevImg.jsx
+++ components/DevImg.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import Image from "next/image";
 
-const DevImg = () => {
+const DevImg = ({ containerStyles, imgSrc }) => {
   return (
-    <div>
-      
+    <div className={`${containerStyles}`}>
+      <Image src={imgSrc} fill priority alt='' />
     </div>
-  )
-}
+  );
+};
 
-export default DevImg
+export default DevImg;
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+'use client'
 
-const Socials = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import {
+  RiYoutubeFill,
+  RiLinkedinFill,
+  RiGithubFill,
+  RiFacebookFill,
+  RiInstagramFill,
+} from 'react-icons/ri';
 
-export default Socials
+import Link from 'next/link';
+
+const icons = [
+  {
+    path: '/',
+    name: <RiYoutubeFill />
+  },
+  {
+    path: '/',
+    name: <RiLinkedinFill />
+  },
+  {
+    path: '/',
+    name: <RiGithubFill />
+  },
+  {
+    path: '/',
+    name: <RiFacebookFill />
+  },
+  {
+    path: '/',
+    name: <RiInstagramFill />
+  },
+];
+
+import React from 'react';
+
+const Socials = ({containerStyles, iconsStyles}) => {
+  return <div className={`${containerStyles}`}>
+    {icons.map((icon, index) => {
+      return <Link href={icon.path} key={index}>
+      <div className={`${iconsStyles}`}>{icon.name}</div></Link>
+    })}
+  </div>;
+};
+
+export default Socials;
